a04758ff79edd1f64e1078ad387fc3969c307e67
home <home@homes-MacBook-Pro-3.local>
2017-07-08 16:28:37 -0700

    Add content to issues controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,20 @@
 class IssuesController < ApplicationController
+	def new
+		@issue = Issue.new
+	end
+
 	def create
-		
+		@issue = Issue.new(issue_params)
+		if @issue.save
+			redirect_to root_path
+		else
+			@errors = @issues.errors.full_messages
+			render "new"
+		end
+	end
+
+	private
+	def issue_params
+		params.require(:booking).permit(:issuable_type, :issuable_id)
 	end
 end
