487ae816aeef3b69249e4a76b2c1da92da4c594b
home <home@homes-MacBook-Pro-3.local>
2017-07-08 16:57:04 -0700

    Add content to requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,29 +1,48 @@
 class RequestsController < ApplicationController
 	def index
-		
+		@requests = Request.all
 	end
 
 	def show
-		
+		@request = Request.find(params[:id])
 	end
 
 	def new
-		
+		@request = Request.new
 	end
 
 	def create
-		
+		@request = Request.new(request_params)
+		if @request.save
+			redirect_to @request
+		else
+			@errors = @request.errors.full_messages
+			render "new"
+		end
 	end
 
 	def edit
-		
+		@request = Request.find(params[:id])
 	end
 
 	def update
-		
+		@request = Request.find(params[:id])
+		if @request.update
+			redirect_to @request
+		else
+			@errors = @request.errors.full_messages
+			render "edit"
+		end
 	end
 
 	def destroy
-		
+		@request = Request.find(params[:id])
+		@request.destroy
+		redirect_to root_path
+	end
+
+	private
+	def request_params
+		params.require(:request).permit(:organization_id, :patient_id, :time, :location, :notes)
 	end
 end
