9f1dc171b883c9ec474948b88a8fd0232d88e570
home <home@homes-MacBook-Pro-3.local>
2017-07-08 15:58:58 -0700

    Add content to blacklist controller

--- app/controllers/blacklists_controller.rb
+++ app/controllers/blacklists_controller.rb
@@ -1,9 +1,16 @@
 class BlacklistsController < ApplicationController
 	def create
-		
+		@blacklist = Blacklist.create(blacklist_params)
+		redirect_to root_path
 	end
 
 	def destroy
-		
+		Blacklist.find(params[:id]).destroy
+		redirect_to root_path
+	end
+
+	private
+	def blacklist_params
+		params.require(:blacklist).permit(:therapist_id)
 	end
 end
