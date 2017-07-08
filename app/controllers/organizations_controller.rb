43a702c1e1e3dac7294db064ceada6f7acc0630b
home <home@homes-MacBook-Pro-3.local>
2017-07-08 16:44:46 -0700

    Add content to organizations controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,29 +1,48 @@
 class OrganizationsController < ApplicationController
 	def index
-		
+		@organizations = Organization.all
 	end
 
 	def show
-		
+		@organization = Organization.find(params[:id])
 	end
 
 	def new
-		
+		@organization = Organization.new
 	end
 
 	def create
-		
+		@organization = Organization.new(organization_params)
+		if @organization.save
+			redirect_to @organization
+		else
+			@errors = @organization.errors.full_messages
+			render "new"
+		end
 	end
 
 	def edit
-		
+		@organization = Organization.find(params[:id])
 	end
 
 	def update
-		
+		@organization = Organization.find(params[:id])
+		if @organization.update(organization_params)
+			redirect_to @organization
+		else
+			@errors = @organization.errors.full_messages
+			render "edit"
+		end
 	end
 
 	def destroy
-		
+		@organization = Organization.find(params[:id])
+		@organization.destroy
+		redirect_to root_path
+	end
+
+	private
+	def organization_params
+		params.require(:organization).permit(:name, :contact, :phone, :email, :notes, :username, :password)
 	end
 end
