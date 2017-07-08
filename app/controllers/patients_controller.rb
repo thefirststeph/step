02716cec6eb9fb2f5d85e81ba6dc1997719281f7
home <home@homes-MacBook-Pro-3.local>
2017-07-08 16:51:12 -0700

    Add content to patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,29 +1,50 @@
 class PatientsController < ApplicationController
 	def index
-		
+		@patients = Patient.all
 	end
 
 	def show
-		
+		@patient = Patient.find(params[:id])
 	end
 
 	def new
-		
+		@patient = Patient.new
 	end
 
 	def create
-		
+		@patient = Patient.new(patient_params)
+		if @patient.save
+			redirect_to @patient
+		else
+			@errors = @patient.errors.full_messages
+			render "new"
+		end
 	end
 
 	def edit
-		
+		@patient = Patient.find(params[:id])
+		@organization = @patient.organization
 	end
 
 	def update
-		
+		@patient = Patient.find(params[:id])
+		if @patient.update(patient_params)
+			redirect_to @patient
+		else
+			@errors = @patient.errors.full_messages
+			render "edit"
+		end
 	end
 
 	def destroy
-		
+		@patient = Patient.find(params[:id])
+		@organization = @patient.organization
+		@patient.destroy
+		redirect_to @organization
+	end
+
+	private
+	def patient_params
+		params.require(:patient).permit(:first_name, :last_name, :zip_code, :age, :sex, :language, :organization_id, :notes)
 	end
 end
