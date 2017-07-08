253feca9a0a592ac75d9cf019702be2a873e8e14
home <home@homes-MacBook-Pro-3.local>
2017-07-08 15:37:54 -0700

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root to:"pages#home"
+
+  resources :requests
+
+  resources :organizations do
+  	resources :patients
+  end
+
+  resources :therapists, only: [:show, :new, :create, :edit, :update] do
+  	resources :bookings
+  end
+
 end
